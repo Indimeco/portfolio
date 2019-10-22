bd184dc4b05c7adbe1432c3e972844ab85240869
Indimeco <18373729+Indimeco@users.noreply.github.com>
2019-10-22 17:28:25 +1100

    feat(log): blinky underscore

--- src/components/About/Log/Component.style.js
+++ src/components/About/Log/Component.style.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { heading, view, center } from '../../UI/snippets';
 import { FONT_LARGE } from '../../UI/base';
 import PropTypes from 'prop-types';
@@ -26,4 +26,24 @@ export const EntryIcon = styled.span`
 	margin: 0 ${SPACE_MEDIUM};
 `;
 
-export const Blink = styled.span``;
+const blinkAnimation = keyframes`
+	0% {
+		opacity: 1;
+	}
+	20% {
+		opacity: 0;
+	}
+	40% {
+		opacity: 0;
+	}
+	60% {
+		opacity: 1;
+	}
+	100% {
+		opacity: 1;
+	}
+`;
+
+export const Blink = styled.span`
+	animation: 1s ${blinkAnimation} ease-out infinite;
+`;
