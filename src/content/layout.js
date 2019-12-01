26b23058f24988b8d31ef8e54735059f9eb114b9
Indimeco <18373729+Indimeco@users.noreply.github.com>
2019-12-01 15:09:22 +1100

    chore(repository): rename repo to manage future portfolios better

--- src/content/layout.js
+++ src/content/layout.js
@@ -1,4 +1,4 @@
-export const portfolioSourceCode = 'https://github.com/Indimeco/portfolio-2019';
+export const portfolioSourceCode = 'https://github.com/Indimeco/portfolio';
 
 export const sections = {
 	links: ['About', 'Portfolio', 'Contact'],
