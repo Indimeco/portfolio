0d79de401e06f533c51a32357947864757c170a5
Indimeco <18373729+Indimeco@users.noreply.github.com>
2021-03-04 08:11:03 +1100

    refactor: wip perspective drawing

--- src/components/Heading/canvas/drawing.ts
+++ src/components/Heading/canvas/drawing.ts
@@ -22,12 +22,15 @@
 type Coordinate = { x: number; y: number };
 type Coordinate3D = Coordinate & { z: number };
 type Polygon = {
-	origin: Coordinate;
 	width: number;
 	height: number;
-	color?: string;
+	color: string;
 };
-type Polygon3D = Polygon & { origin: Coordinate3D; depth: number; vanishingPoint: Coordinate };
+type Polygon3D = Polygon & { depth: number };
+type PicturePlane = {
+	vanishingPoint: Coordinate;
+	observerDistanceFromPicturePlane: number;
+};
 const tracePolygon = (ctx: CanvasRenderingContext2D, ...coords: Coordinate[]) => {
 	if (coords.length <= 0) return;
 
@@ -37,27 +40,6 @@ const tracePolygon = (ctx: CanvasRenderingContext2D, ...coords: Coordinate[]) =>
 		ctx.lineTo(x, y);
 	});
 	ctx.closePath();
-};
-const drawRectangularPlane = (
-	ctx: CanvasRenderingContext2D,
-	{ origin, width, height, color = 'pink' }: Polygon,
-) => {
-	const destination = {
-		x: origin.x + width,
-		y: origin.y + height,
-	};
-
-	tracePolygon(
-		ctx,
-		{ x: origin.x, y: origin.y },
-		{ x: destination.x, y: origin.y },
-		{ x: destination.x, y: destination.y },
-		{ x: origin.x, y: destination.y },
-		{ x: origin.x, y: origin.y },
-	);
-
-	ctx.strokeStyle = color;
-	ctx.stroke();
 };
 function getRectangularPlane({
 	origin,
@@ -80,54 +62,50 @@ function getRectangularPlane({
 		{ x: origin.x, y: origin.y, z: origin.z },
 	];
 }
-// This formula i got from https://math.stackexchange.com/questions/2337183/one-point-perspective-formula doesn't seem to be correct
-// a point scaled at 0, 0 to a vanishing point at 450 becomes 0, 0
-const scaleCoordinate = (
-	origin: Coordinate3D,
-	vanishingPoint: Coordinate,
-	observerDepth = -30,
-): Coordinate => {
-	const scaled = {
-		x: vanishingPoint.x + (origin.x - vanishingPoint.x) * (observerDepth / (observerDepth + origin.z)),
-		y: vanishingPoint.y + (origin.y - vanishingPoint.y) * (observerDepth / (observerDepth + origin.z)),
-	};
-	console.log('Origin: ', origin);
-	console.log('Scaled: ', scaled);
-	return scaled;
-};
 
-// Assuming that the picture plane is located at (0,0,0) in 3D space
-function convert3DCoordinateToPicturePlane(coordinate: Coordinate3D): Coordinate {
-	const distanceFromObserverToPicturePlane = 10;
+function convert3DCoordinateToPicturePlane(
+	vanishingPoint: Coordinate,
+	observerDistanceFromPicturePlane: number,
+	coordinate: Coordinate3D,
+): Coordinate {
 	return {
 		x:
-			(distanceFromObserverToPicturePlane * coordinate.x) /
-			(coordinate.z + distanceFromObserverToPicturePlane),
+			vanishingPoint.x +
+			(coordinate.x - vanishingPoint.x) *
+				(observerDistanceFromPicturePlane / (observerDistanceFromPicturePlane + coordinate.z)),
 		y:
-			(distanceFromObserverToPicturePlane * coordinate.y) /
-			(coordinate.z + distanceFromObserverToPicturePlane),
+			vanishingPoint.y +
+			(coordinate.y - vanishingPoint.y) *
+				(observerDistanceFromPicturePlane / (observerDistanceFromPicturePlane + coordinate.z)),
 	};
 }
-const drawRectangularPrism = (
+function drawRectangularPrism(
 	ctx: CanvasRenderingContext2D,
-	{ origin, width, height, depth, vanishingPoint }: Polygon3D,
-) => {
+	{ vanishingPoint, observerDistanceFromPicturePlane }: PicturePlane,
+	{ width, depth, height, color }: Polygon3D,
+	origin: Coordinate3D,
+) {
+	// NOTE should curry many of these functions
+
 	tracePolygon(
 		ctx,
-		...getRectangularPlane({ origin: { x: 100, y: 200, z: 0 }, width: 100, height: 200 }).map(
-			convert3DCoordinateToPicturePlane,
+		...getRectangularPlane({ origin: { x: origin.x, y: origin.y, z: origin.z }, width, height }).map((o) =>
+			convert3DCoordinateToPicturePlane(vanishingPoint, observerDistanceFromPicturePlane, o),
 		),
 	);
-	ctx.strokeStyle = 'yellow';
+	ctx.strokeStyle = color;
 	ctx.stroke();
-	drawRectangularPlane(ctx, { origin, width, height, color: 'blue' });
-	drawRectangularPlane(ctx, {
-		origin: scaleCoordinate(origin, vanishingPoint),
-		width: width - depth,
-		height: height - depth,
-		color: 'green',
-	});
-};
+	tracePolygon(
+		ctx,
+		...getRectangularPlane({
+			origin: { x: origin.x, y: origin.y, z: origin.z + depth },
+			width,
+			height,
+		}).map((o) => convert3DCoordinateToPicturePlane(vanishingPoint, observerDistanceFromPicturePlane, o)),
+	);
+	ctx.strokeStyle = color;
+	ctx.stroke();
+}
 
 export function drawing(canvas: HTMLCanvasElement): void {
 	const ctx = canvas.getContext('2d');
@@ -140,19 +118,38 @@ export function drawing(canvas: HTMLCanvasElement): void {
 	// eslint-disable-next-line no-param-reassign
 	canvas.height = canvasHeight;
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	const vanishingPoint: Coordinate = { x: canvas.width / 2, y: canvas.height / 2 };
+	const vanishingPoint: Coordinate = { x: canvas.width - canvas.width / 4, y: canvas.height / 4 };
+	const observerDistanceFromPicturePlane = 1000;
 	console.group();
 	console.log('Canvas Width: ', canvas.width);
 	console.log('Canvas Height: ', canvas.height);
 	console.log('Vanishing Point: ', vanishingPoint);
 	console.groupEnd();
 
-	drawRectangularPrism(ctx, {
-		origin: { x: 0, y: 0, z: 0 },
-		width: 100,
-		height: 300,
-		depth: 30,
-		vanishingPoint,
-		color: 'yellow',
-	});
+	// Draw the vanishing point
+	tracePolygon(
+		ctx,
+		...getRectangularPlane({
+			origin: { x: vanishingPoint.x, y: vanishingPoint.y, z: -1 },
+			width: 2,
+			height: 2,
+		}).map((o) => ({ x: o.x, y: o.y })),
+	);
+	ctx.fillStyle = 'red';
+	ctx.fill();
+
+	drawRectangularPrism(
+		ctx,
+		{
+			vanishingPoint,
+			observerDistanceFromPicturePlane,
+		},
+		{
+			width: 100,
+			height: 200,
+			depth: 100,
+			color: 'yellow',
+		},
+		{ x: 0, y: 0, z: 0 },
+	);
 }
