a34dc4636ecadf165edf7d66f50782848de27411
Indimeco <18373729+Indimeco@users.noreply.github.com>
2021-07-10 19:22:10 +1000

    refactor(drawing): re-enable anti aliasing

--- src/components/CanvasDrawing/drawing/polygons.ts
+++ src/components/CanvasDrawing/drawing/polygons.ts
@@ -2,10 +2,6 @@ import { curry } from 'ramda';
 
 import { Coordinate } from './types';
 
-function withoutSubpixelRendering(coord: Coordinate): Coordinate {
-	return { x: Math.round(coord.x), y: Math.round(coord.y) };
-}
-
 function getCanvasPosition(canvas: HTMLCanvasElement) {
 	return Math.round(canvas.parentElement?.offsetTop ?? 0);
 }
@@ -14,12 +10,12 @@ function _tracePolygon(ctx: CanvasRenderingContext2D, ...coords: Coordinate[]) {
 	if (coords.length <= 0) return;
 
 	const canvasOffsetTop = getCanvasPosition(ctx.canvas);
-	const firstCoord = withoutSubpixelRendering(coords[0]);
+	const firstCoord = coords[0];
 
 	ctx.beginPath();
 	ctx.moveTo(firstCoord.x, firstCoord.y - canvasOffsetTop);
 	coords.forEach((coord) => {
-		const { x, y } = withoutSubpixelRendering(coord);
+		const { x, y } = coord;
 		ctx.lineTo(x, y - canvasOffsetTop);
 	});
 	ctx.closePath();
