f39aeb538341424fe4c56a5c3d068eaa80cb8136
Indimeco <18373729+Indimeco@users.noreply.github.com>
2021-07-11 15:01:56 +1000

    chore(about, portfolio): add some placeholder content

--- src/content/about.js
+++ src/content/about.js
@@ -1,4 +1,4 @@
-import { faReact, faNode } from '@fortawesome/free-brands-svg-icons';
+import { faReact, faJs } from '@fortawesome/free-brands-svg-icons';
 import { faCodeBranch } from '@fortawesome/free-solid-svg-icons';
 
 export const rotary = {
@@ -6,7 +6,18 @@ export const rotary = {
 	words: ['sustainability', 'communication', 'extensibility'],
 };
 export const about = [
-	["Hi, I'm a self taught web developer", faCodeBranch, 'from the USA'],
-	['I work in the React', faReact, 'and Node', faNode, 'ecosystem'],
+	["Hi, I'm a self taught software engineer", faCodeBranch, 'living and working in Australia'],
+	[
+		'I specialize in all things',
+		faJs,
+		'JavaScript both in the browser and in the cloud, but my favorite tools are React',
+		faReact,
+		'and Typescript',
+	],
+	[
+		'I have lead the development of multiple design systems that form the backbone of large FinTech and Hospitality user experiences.',
+	],
+	['Thousands of people use payment integrations I wrote.'],
+	['I live a life of learning. I want to build great experiences. I want to empower people'],
 ];
 export const aboutId = 'about';
